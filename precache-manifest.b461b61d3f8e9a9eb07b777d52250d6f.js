f891888b869af3f6b1a1b9a4457bfec43b86c76e
Jaffer Syed <JafferSyed@Jaffers-MacBook-Pro.local>
2020-02-04 02:11:45 -0500

    Updates

---
+++ precache-manifest.b461b61d3f8e9a9eb07b777d52250d6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5804c60425505644a4419d34b417886d",
+    "url": "/JPI-web-page/index.html"
+  },
+  {
+    "revision": "9dc97b6a1700c31a1b82",
+    "url": "/JPI-web-page/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "630b22afd21dbb076351",
+    "url": "/JPI-web-page/static/css/main.af5f9813.chunk.css"
+  },
+  {
+    "revision": "9dc97b6a1700c31a1b82",
+    "url": "/JPI-web-page/static/js/2.d07f097e.chunk.js"
+  },
+  {
+    "revision": "630b22afd21dbb076351",
+    "url": "/JPI-web-page/static/js/main.4280896d.chunk.js"
+  },
+  {
+    "revision": "569f72e3ea4350dc2380",
+    "url": "/JPI-web-page/static/js/runtime-main.bfd1352a.js"
+  }
+]);
